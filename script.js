2df376c1fb542a8a10aa34ffce42c67f86616c73
Stephen Talorong <talorong0419@gmail.com>
2020-12-30 23:15:10 +0800

    menu-nav

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function openMenu() {
-    
+const menu = document.querySelector('.menu');
+const menu_li = document.querySelectorAll('.nav');
+const intro = document.querySelector('.intro');
+const openMenu = document.querySelector('.open-menu');
+const subIntro = document.querySelector('.sub-intro');
+
+console.log(menu_li);
+
+function toggleMenu() {
+    // intro.style.display = 'none';
+    // intro.innerHTML = '';
+    intro.classList.toggle('hide');
+    subIntro.classList.toggle('hide');
+    menu.classList.toggle('animate-menu');
+    menu_li.forEach(item => {
+    item.classList.toggle('animate-li');
+    });
 }
