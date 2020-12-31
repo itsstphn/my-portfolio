1112ed65e6ad6e26b4ae52797d164c1faa69e945
Stephen Talorong <talorong0419@gmail.com>
2020-12-31 23:02:02 +0800

    image darken

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const intro = document.querySelector('.intro');
 const openMenu = document.querySelector('.open-menu');
 const subIntro = document.querySelector('.sub-intro');
 const menu_bg = document.querySelector('.menu-bg');
+const hero_image = document.querySelector('.hero-img');
 
+console.log(hero_image);
 
 function toggleMenu() {
 
     menu.classList.toggle('animate-menu');
     menu_bg.classList.toggle('animate-ul');
-    
     // openMenu.classList.toggle('animate-ul');
     menu_li.forEach(item => {
         item.classList.toggle('animate-li');
     })
+    hero_image.classList.toggle('image-darken');
 }
